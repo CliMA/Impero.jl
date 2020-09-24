3d4b5cc50e4fbd86bc2cb6b323a82330557c46ea
sandreza <andrenogueirasouza@gmail.com>
2020-09-23 21:51:06 -0400

    changes n_name -> b_name

--- src/symbolic_utils_hooks.jl
+++ src/symbolic_utils_hooks.jl
@@ -2,7 +2,7 @@ using SymbolicUtils
 import SymbolicUtils: Chain, Postwalk
 import SymbolicUtils: Sym, Term, istree, operation, arguments, to_symbolic, Fixpoint
 
-include("abstract_operations.jl")
+# include("abstract_operations.jl")
 
 for unary_operator in unary_operators
     b_name, b_symbol = Meta.parse.(unary_operator)
@@ -23,7 +23,7 @@ end
 
 # Define Struct and Symbol Overload for n-ary Operators
 for nary_operator in nary_operators
-    n_name, n_symbol = Meta.parse.(nary_operator)
+    b_name, b_symbol = Meta.parse.(nary_operator)
     @eval SymbolicUtils.istree(a::$b_name) = true
     @eval SymbolicUtils.arguments(a::$b_name) = [a.terms...] 
     @eval SymbolicUtils.operation(a::$b_name) = $b_symbol
