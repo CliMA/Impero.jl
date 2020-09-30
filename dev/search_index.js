499bb6754be1b826f40c0b1a76c4e9603e2f9457
Documenter.jl <documenter@juliadocs.github.io>
2020-09-30 14:42:15 +0000

    build based on fdde812

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Impero.jl","page":"Impero","title":"Impero.jl","text":"","category":"section"},{"location":"","page":"Impero","title":"Impero","text":"Documentation for Impero.jl","category":"page"}]
+}
