23f2832e9e7294b183fff8aef925930f6228c311
sandreza <andrenogueirasouza@gmail.com>
2020-10-31 17:08:53 -0400

    finishing up preliminary sketch

--- test/domain_test.jl
+++ test/domain_test.jl
@@ -33,6 +33,11 @@ function ∂(a::IntervalDomain)
     return nothing
 end
 import Impero: ndims
+import Base: show
+function Base.show(io::IO, o::PointBoundary)
+    print("{",o.point,"}")
+end
+
 ndims(p::PointBoundary) = 0
 function ∂(p::ProductDomain)
     boundaries = []
@@ -43,4 +48,25 @@ function ∂(p::ProductDomain)
 end
 
 ∂(Interval(-1,1))
-∂Ω = ∂(Ω×Ω)
+∂Ω = ∂(Ω)
+function futureconstructor(Ω)
+    ∂Ω = ∂(Ω)
+    splitb = []
+    for (i,boundary) in enumerate(∂Ω)
+        tmp = Any[]
+        push!(tmp, Ω.domains...)
+        if boundary != nothing
+            tmp1 = copy(tmp)
+            tmp2 = copy(tmp)
+            tmp1[i] = boundary[1]
+            push!(splitb, (tmp1))
+            tmp2[i] = boundary[2]
+            push!(splitb, (tmp2))
+        end
+    end
+    return splitb
+end
+boundaries = futureconstructor(Ω);
+for i in eachindex(boundaries)
+    println(boundaries[i][1], "×",boundaries[i][2])
+end
