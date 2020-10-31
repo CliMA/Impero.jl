5c8ea3c9c998369def76748a8dda068d7226328e
sandreza <andrenogueirasouza@gmail.com>
2020-10-31 16:22:05 -0400

    preliminary attempts on boundaries

--- test/domain_test.jl
+++ test/domain_test.jl
@@ -12,3 +12,35 @@ info(Ω)
     @test ndims(t1) == 3
     @test Ω.domains[1] == Ω[1]
 end
+
+abstract type AbstractBoundary end
+
+struct PointBoundary{S}
+    point::S
+end
+
+struct Boundaries{S}
+    boundaries::S
+end
+
+
+function ∂(a::IntervalDomain)
+    if a.periodic
+        return (nothing)
+    else
+        return (PointBoundary(a.a), PointBoundary(a.b))
+    end
+    return nothing
+end
+import Impero: ndims
+ndims(p::PointBoundary) = 0
+function ∂(p::ProductDomain)
+    boundaries = []
+    for domain in p.domains
+        push!(boundaries, ∂(domain))
+    end
+    return boundaries
+end
+
+∂(Interval(-1,1))
+∂Ω = ∂(Ω×Ω)
